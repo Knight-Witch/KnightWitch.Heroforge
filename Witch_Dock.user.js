6ceb4bfd0acf17a4d1ecec18de5822cd2c06506a
Knight Witch <amanda.m.ivans@gmail.com>
2025-12-29 20:24:05 -0800

    Create Witch_Dock.user.js

---
+++ Witch_Dock.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Witch Dock (REQUIRED)
+// @namespace    body-editor
+// @version      0.1.0
+// @description  This dock is the REQUIRED for my scripts. This provides the UI in which various KW scripts will populate. It serves no function on its own without any of the dependant scripts installed. Please refer to this document for a w
+// @match        https://www.heroforge.com/*
+// @match        https://heroforge.com/*
+// @grant        unsafeWindow
+// @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/Knight-Witch/KnightWitch.Heroforge/main/Witch_Dock.user.js
+// @downloadURL  https://raw.githubusercontent.com/Knight-Witch/KnightWitch.Heroforge/Witch_Dock.user.js
+// ==/UserScript==
