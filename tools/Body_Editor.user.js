0ce4bf22557e317d24e3976224e8ee7f5f6495dc
Knight Witch <amanda.m.ivans@gmail.com>
2026-01-12 19:02:44 -0800

    Create Body_Editor.user.js

---
+++ tools/Body_Editor.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Body Editor v1
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0-wdtest1
+// @description  Body Editor tool registered into Witch Dock (WITCH DOCK TEST RUN).
+// @match        https://www.heroforge.com/*
+// @match        https://heroforge.com/*
+// @grant        unsafeWindow
+// @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/Knight-Witch/KnightWitch.Heroforge/Witch_Scripts/Body_Editor.user.js
+// @downloadURL  https://raw.githubusercontent.com/Knight-Witch/KnightWitch.Heroforge/Witch_Scripts/Body_Editor.user.js
+// ==/UserScript==
+
+// PLACEHOLDER
