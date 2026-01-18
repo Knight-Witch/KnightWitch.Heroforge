bb9362c5208e1aaf5b42c6a8ca342ecdd60da793
Knight Witch <amanda.m.ivans@gmail.com>
2026-01-18 01:23:46 -0800

    Update Witch_Dock.user.js

--- Witch_Dock.user.js
+++ Witch_Dock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Witch Dock v1
 // @namespace    KnightWitch
-// @version      1.0.2
+// @version      1.0.3
 // @description  UI for all Witch Scripts - The official release!
 // @match        https://www.heroforge.com/*
 // @match        https://heroforge.com/*
