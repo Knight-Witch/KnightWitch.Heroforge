7b1d3eda5e2570bd76cfb25930206af3bc7026fb
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-03 04:45:11 -0700

    Load HF UI slot bridge module

--- HeroForge_UI/HF_UI_Slot_Bridge.js
+++ HeroForge_UI/HF_UI_Slot_Bridge.js
@@ -1,3 +1,21 @@
 (function () {
   "use strict";
+
+  const url = "https://raw.githubusercontent.com/Knight-Witch/KnightWitch.Heroforge/Witch_Scripts/HeroForge_UI/Expanded_" + "Decal" + "_Slots.js";
+
+  function run(code) {
+    if (!code) return;
+    new Function(code)();
+  }
+
+  if (typeof GM_xmlhttpRequest === "function") {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url,
+      headers: { "Cache-Control": "no-cache" },
+      onload: (res) => {
+        if (res.status >= 200 && res.status < 300) run(res.responseText || "");
+      }
+    });
+  }
 })();
